54fbfe2217940b7fcce28b8f12e1da07553acd11
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-10-26 20:08:15 +0200

    implemento ciclo for.length

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,30 +25,37 @@ let cellNumbers = 100
 // Ogni cella ha un numero progressivo, da 1 a 100.
     // Generiamo dei numeri da 1 a 100 
 
-buttonElement.addEventListener(`click`,function () {
-    // console.log(`Ho cliccato sul pulsante`);
-
-    for (let i = 1; i <= cellNumbers; i++) {
-        // console.log(cellNumbers);
-        // creiamo il nostro markup attraverso una costante  da inserire nella dom 
-        let cellElement = `<div class="cell">${i}</div>`
-        // Utilizziamo insertAd
-        gridElement.insertAdjacentHTML('beforeend', cellElement); 
-    }    
-    // Quando l'utente cliccherà sulla cella essa si deve colorare di blu
-       // selezionare la singola cella
-       // vedere se la cessa selezionata corrisponda 
-       const singleCell = document.querySelector(`.cell`);
-       // console.log(singleCell);
-
-       // utliazziamo un eventListner 
-         // aggiungo un event listener alla cella selezionata
-        singleCell.addEventListener(`click`,function () {
-        console.log(`ho cliccato`);
-         // al click la cella si colorerà di blu
-        singleCell.classList.toggle(`lightblue`)
-        })  
-
-})    
-
+    buttonElement.addEventListener(`click`,function () {
+        // console.log(`Ho cliccato sul pulsante`);
     
+        for (let i = 1; i <= cellNumbers; i++) {
+            // console.log(cellNumbers);
+            // creiamo il nostro markup attraverso una costante  da inserire nella dom 
+            let cellElement = `<div class="cell">${i}</div>`
+            // Utilizziamo insertAd
+            gridElement.insertAdjacentHTML('beforeend', cellElement); 
+        }    
+        // Quando l'utente cliccherà sulla cella essa si deve colorare di blu
+           // selezionare la singola cella
+           // vedere se la cella selezionata corrisponda 
+           const selectedCell = document.querySelectorAll(`.cell`);
+           // console.log(singleCell);
+           
+           // Creiamo un ciclo for .length per ciclare all'interno 
+           for (let i = 1; i <= selectedCell.length; i++) {
+               const singleCell = selectedCell[i];
+                 // console.log(singleCell);
+               
+               // utliazziamo un eventListner 
+                 // aggiungo un event listener alla cella selezionata
+                singleCell.addEventListener(`click`,function () {
+                 // console.log(`ho cliccato`);
+
+                 // al click la cella si colorerà di blu
+                singleCell.classList.toggle(`lightblue`);
+                })  
+                 // stampiamo in console la cella selezionata 
+                console.log(`la cella selezionata è ${singleCell.innerHTML}`);
+           }
+    
+    })    
